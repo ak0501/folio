db994ee45fbfade9ed942a40f4d6ed739872dcdf
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-09-15 09:36:38 -0500

    cleanup

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-require('typeface-roboto');
+import React from "react";
+require("typeface-roboto");
 
-const Paragraph=() => {
-  return(
-      <>
-       <p>
-       Aspiring full-stack web developer with a design engineering background of over eight years of experience. Currently taking a full stack developer certification course at the University of Minnesota. With each project, becoming more efficient using a wide variety of web development tools to create meaningful applications. Excel at visualizing how things work and using web development tools along with extensive design background to achieve a great solution.Worked extensively with customers on multi-million dollar projects to design one of a kind equipment for their specific needs. Ready-set to add value onboard your team! 
-       </p>
-      </>
-  )
-}
+const Paragraph = () => {
+  return (
+    <>
+      <p>
+        {/* Aspiring full-stack web developer with a mechanical design engineering
+        background of over eight years of experience. I just completed a full
+        stack developer certification course from the University of Minnesota.
+        With each project, becoming more efficient using a wide variety of web
+        development tools to create meaningful applications. Excel at
+        visualizing how things work and using web development tools along with
+        extensive design background to achieve a great solution.Worked
+        extensively with customers on multi-million dollar projects to design
+        one of a kind equipment for their specific needs. Ready-set to add value
+        onboard your team! */}
+        
+      </p>
+    </>
+  );
+};
 
 export default Paragraph;
