2327a491d8671802da9938e38542ce0de5f7fe32
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-09-26 20:58:33 -0500

    Added Temp marker

--- src/components/LocalWeather.jsx
+++ src/components/LocalWeather.jsx
@@ -6,7 +6,7 @@ const api = {
   url: process.env.REACT_APP_API_URL,
 };
 
-const API_KEY = "f2d7a3ee16ff14d75f0702f3db41469b";
+const API_KEY = "abb1cf1535bd78a8e3431b6fa9b448eb";
 const API_URL = "http://api.openweathermap.org/data/2.5/weather";
 const LocalWeather = (props) => {
   const [lat, setLat] = useState("");
@@ -14,44 +14,57 @@ const LocalWeather = (props) => {
   const [city, setCity] = useState("");
   const [ico, setIco] = useState("");
   const [desc, setDesc] = useState("");
+  const [temp, setTemp] = useState("");
   let imgUrl = `http://openweathermap.org/img/wn/${ico}.png`;
 
   /* -------------------------- useeffect to call API ------------------------- */
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
+      // console.log(position);
       setLat(position.coords.latitude);
       setLon(position.coords.longitude);
+      const data = fetch(
+        `${API_URL}?lat=${lat}&lon=${lon}&unit=imperial&appid=${API_KEY}`
+      )
+        .then((data) => data.json())
+        .then((data) => {
+          if (data.cod !== 200) {
+            throw new Error();
+          }
+
+          setCity(data.name);
+          setIco(data.weather[0].icon);
+          setDesc(data.weather[0].description);
+          setTemp(data.main.temp);
+
+          // console.log(city);
+          // console.log(ico);
+
+          // setLoading(false);
+        })
+        .catch((err) => {
+          // setError(true);
+          // setLoading(false);
+          console.log(err.message);
+        });
     });
   }, []);
 
-  fetch(`${API_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-    .then((response) => response.json())
-    .then((response) => {
-      if (response.cod !== 200) {
-        throw new Error();
-      }
+  useEffect(() => {}, []);
 
-      setCity(response.name);
-      setIco(response.weather[0].icon);
-      setDesc(response.weather[0].description);
-      console.log(city);
-      console.log(ico);
-
-      // setLoading(false);
-    })
-    .catch((err) => {
-      // setError(true);
-      // setLoading(false);
-      console.log(err.message);
-    });
   return (
     <>
       <div>
-        <p> Your location is {city}</p>
+        <p>
+          {" "}
+          Location{""}
+          {city}
+        </p>
         <a>
           <img src={imgUrl} alt="day" />
+          <p></p>
           {desc}
+          {`${temp}F`}
         </a>
       </div>
     </>
