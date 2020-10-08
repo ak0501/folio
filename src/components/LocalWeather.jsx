3141161308cf3b6b3570b465ce7b649a2a13129d
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-10-08 10:15:56 -0500

    Added weather API key

--- src/components/LocalWeather.jsx
+++ src/components/LocalWeather.jsx
@@ -6,7 +6,7 @@ require("dotenv").config();
 //   url: process.env.REACT_APP_API_URL,
 // };
 
-const API_KEY = "";
+const API_KEY = "f2d7a3ee16ff14d75f0702f3db41469b";
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 const LocalWeather = () => {
   const [lat, setLat] = useState("");
@@ -19,7 +19,7 @@ const LocalWeather = () => {
 
   /* -------------------------- useeffect to call API ------------------------- */
   useEffect(() => {
-     navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition((position) => {
       // console.log(position);
       setLat(position.coords.latitude);
       setLon(position.coords.longitude);
@@ -55,16 +55,11 @@ const LocalWeather = () => {
   return (
     <>
       <div>
-        <p>
-          {" "}
-          Location{" "}
-          {city}
-        </p>
+        <p> Location {city}</p>
         <a>
           <img src={imgUrl} alt="day" />
           <p></p>
-          {desc}{" "}
-          {`${temp}F`}
+          {desc} {`${temp}F`}
         </a>
       </div>
     </>
