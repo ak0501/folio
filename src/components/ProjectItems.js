e3dfe15c4b489d35d0c7cc8bb0a776bebddae908
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-10-08 16:11:36 -0500

    added information to card

--- src/components/ProjectItems.js
+++ src/components/ProjectItems.js
@@ -2,27 +2,38 @@
 export const ProjectItems = [
          {
            Title: "LilOnes",
-           imgUrl: "/public/images/lilones.jpg",
+           image: "/public/images/lilones.jpg",
            route: "/",
            id: "lil",
-           
+           description: "Daycare Management App",
+           paragraph:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ligula leo, pharetra eu faucibus at, sodales at erat. Suspendisse potenti. Donec malesuada neque vitae diam varius, nec auctor dui pellentesque. Etiam non justo quis enim finibus interdum. Vestibulum bibendum nulla et feugiat lobortis. Fusce pretium, nunc vel bibendum semper, leo justo gravida arcu, nec accumsan urna velit vitae nisi. Integer et sem sed sem euismod lacinia sed vitae metus.",
          },
          {
            Title: "Weather",
-           imgUrl: "/public/images/lilones.jpg",
+           image: "/public/images/lilones.jpg",
            route: "/",
            id: "Wea",
+           description: "Daycare Management App",
+           paragraph:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ligula leo, pharetra eu faucibus at, sodales at erat. Suspendisse potenti. Donec malesuada neque vitae diam varius, nec auctor dui pellentesque. Etiam non justo quis enim finibus interdum. Vestibulum bibendum nulla et feugiat lobortis. Fusce pretium, nunc vel bibendum semper, leo justo gravida arcu, nec accumsan urna velit vitae nisi. Integer et sem sed sem euismod lacinia sed vitae metus.",
          },
          {
            Title: "ToDO",
-           imgUrl: "/public/images/lilones.jpg",
+           image: "/public/images/lilones.jpg",
            route: "/",
            id: "TD",
+           description: "Daycare Management App",
+           paragraph:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ligula leo, pharetra eu faucibus at, sodales at erat. Suspendisse potenti. Donec malesuada neque vitae diam varius, nec auctor dui pellentesque. Etiam non justo quis enim finibus interdum. Vestibulum bibendum nulla et feugiat lobortis. Fusce pretium, nunc vel bibendum semper, leo justo gravida arcu, nec accumsan urna velit vitae nisi. Integer et sem sed sem euismod lacinia sed vitae metus.",
          },
          {
            Title: "CarbTracker",
-           imgUrl: "/public/images/lilones.jpg",
+           image: "/public/images/lilones.jpg",
            route: "/",
            id: "CT",
+           description: "Daycare Management App",
+           paragraph:
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ligula leo, pharetra eu faucibus at, sodales at erat. Suspendisse potenti. Donec malesuada neque vitae diam varius, nec auctor dui pellentesque. Etiam non justo quis enim finibus interdum. Vestibulum bibendum nulla et feugiat lobortis. Fusce pretium, nunc vel bibendum semper, leo justo gravida arcu, nec accumsan urna velit vitae nisi. Integer et sem sed sem euismod lacinia sed vitae metus.",
          },
        ];
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,12 +4,13 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Card from "./Card";
 import { ProjectItems } from "./ProjectItems";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    height: 140,
+    height: 120,
     width: 100,
   },
   control: {
@@ -25,133 +26,19 @@ const Projects = () => {
             {ProjectItems.map((item, index) => (
               <Grid key={index} item>
                 <Paper className={useStyles.paper} />
-                <Card key={index}
+                <Card
+                  key={index}
                   title={item.Title}
                   avatar={item.id}
-                  
-                  
-                
+                  image={item.image}
+                  description={item.description}
+                  paragraph={item.paragraph}
                 />
               </Grid>
             ))}
           </Grid>
         </Grid>
       </Grid>
-      {/* <div className="row">
-          
-            <div
-              className="col-md-4 animate-box"
-              data-animate-effect="fadeInRight"
-            >
-              <div
-                className="project"
-                style={{ backgroundImage: "url(images/lilOnes.jpg)" }}
-              >
-                <div className="desc">
-                  <div className="con">
-                    <h3>
-                      <a href="https://github.com/ak0501/pequenoOnes">
-                        lilOnes
-                      </a>
-                      pequenoOnes
-                    </h3>
-                    <span></span>
-                    <p className="icon">
-                      <span>
-                        <a href="#">
-                          <i className="icon-share3" />
-                        </a>
-                      </span>
-                      <span>
-                        <a href="#">
-                          <i className="icon-eye" /> 100
-                        </a>
-                      </span>
-                      <span>
-                        <a href="#">
-                          <i className="icon-heart" /> 49
-                        </a>
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div
-              className="col-md-4 animate-box"
-              data-animate-effect="fadeInTop"
-            >
-              <div
-                className="project"
-                style={{ backgroundImage: "url(images/weather.png)" }}
-              >
-                <div className="desc">
-                  <div className="con">
-                    <h3>
-                      <a href="https://ak0501.github.io/Weather-API/">
-                        Weather
-                      </a>
-                    </h3>
-                    <span></span>
-                    <p className="icon">
-                      <span>
-                        <a href="#">
-                          <i className="icon-share3" />
-                        </a>
-                      </span>
-                      <span>
-                        <a href="#">
-                          <i className="icon-eye" /> 100
-                        </a>
-                      </span>
-                      <span>
-                        <a href="#">
-                          <i className="icon-heart" /> 49
-                        </a>
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div
-              className="col-md-4 animate-box"
-              data-animate-effect="fadeInBottom"
-            >
-              <div
-                className="project"
-                style={{ backgroundImage: "url(images/carbTracker.png)" }}
-              >
-                <div className="desc">
-                  <div className="con">
-                    <h3>
-                      <a href="https://appcarbtracker.herokuapp.com/">
-                        CarbTracker
-                      </a>
-                    </h3>
-                    <span></span>
-                    <p className="icon">
-                      <span>
-                        <a href="#">
-                          <i className="icon-share3" />
-                        </a>
-                      </span>
-                      <span>
-                        <a href="#">
-                          <i className="icon-eye" /> 100
-                        </a>
-                      </span>
-                      <span>
-                        <a href="#">
-                          <i className="icon-heart" /> 49
-                        </a>
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
     </div>
   );
 };
