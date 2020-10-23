02d99cac31a6000231bd99843be0e14512f8365d
ak0501 <avinashkhanal@gmail.com>
2020-10-23 14:57:02 -0500

    Added to paragraph

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -5,7 +5,10 @@ const Paragraph = () => {
   return (
     <>
       <p>
-        A junior full stack developer ready-set to add value
+       Originally from Nepal, came to the United States for heigher education. I recsived my degree in Manufacturing Engineering and have been working in 
+       <br/> the field of manufacturing for number of years. 
+        In the summer of 2020, due to COVID pandemic, my place of employement was affected heavily.
+        I went to  A junior full stack developer ready-set to add value
         onboard your team!
         
       </p>
