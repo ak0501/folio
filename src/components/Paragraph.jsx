0bdb54d41c73df5a63d126d6dfff566671217947
ak0501 <avinashkhanal@gmail.com>
2020-10-23 16:09:47 -0500

    corrected spellings

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -5,7 +5,7 @@ const Paragraph = () => {
   return (
     <>
       <p>
-       <strong>O</strong>riginally from Nepal, came to the United States for heigher education. I received my degree in Manufacturing Engineering and have been working in the field of manufacturing for number of years. 
+       <strong>O</strong>riginally from Nepal, I came to the United States in pursuit of higher education. I received my degree in Manufacturing Engineering and have been working in the field of manufacturing for number of years. 
         In the summer of 2020, I took the full advantage of COVID pandemic and joined the University of Minnesota Full-Stack Bootcamp to learn about web technologies.<br/>
         I am actively seeking for employment opportunities a web developer and ready-set to add value onboard your team!
         
